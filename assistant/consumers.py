bc8baf80bea27dafc27d1cc028dc92b3a563af06
Julien Leicher <jleicher@gmail.com>
2018-12-02 11:54:57 +0100

    Added token verification for websockets

--- assistant/consumers.py
+++ assistant/consumers.py
@@ -1,10 +1,13 @@
 from channels.generic.websocket import WebsocketConsumer
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.settings import api_settings
 import json
 
 class AssistantConsumer(WebsocketConsumer):
 
   def connect(self):
     self.accept()
+    self._authenticated = False
 
   def disconnect(self, code):
     pass
@@ -12,6 +15,16 @@ class AssistantConsumer(WebsocketConsumer):
   def receive(self, text_data):
     data = json.loads(text_data)
 
-    self.send(text_data=json.dumps({
-      'message': data['message'],
-    }))
+    if self._authenticated:
+      self.send(text_data=json.dumps({
+        'message': data['message'],
+      }))
+    elif data.get('type') == 'authenticate':
+      try:
+        token = AccessToken(data.get('token'))
+        print ('Authenticated user id', token[api_settings.USER_ID_CLAIM])
+        self._authenticated = True
+      except Exception as e:
+        print (e)
+        self.close()
+
