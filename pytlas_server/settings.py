0c4bf457e81f5da30886bb88bc21fb183941f8a4
Julien LEICHER <jleicher@gmail.com>
2018-12-05 14:14:41 +0100

    Added whitenoise

--- pytlas_server/settings.py
+++ pytlas_server/settings.py
@@ -59,8 +59,9 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
