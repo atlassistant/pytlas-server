00d7a1c2e09dc8b04c8ef355a57697cbcd4bf13b
Julien LEICHER <jleicher@gmail.com>
2019-01-04 13:27:20 +0000

    Mettre à jour 'postinstall.sh'

--- postinstall.sh
+++ postinstall.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo "Which language do you wish to support? (comma separated list without spaces)"
-read languages
+#echo "Which language do you wish to support? (comma separated list without spaces)"
+#read languages
 
-for lang in $(echo $languages | sed "s/,/ /g")
-do
-    # call your procedure/other scripts here below
-    snips-nlu download $lang
-done
+#for lang in $(echo $languages | sed "s/,/ /g")
+#do
+#    snips-nlu download $lang
+#done
 
 echo "Creating super user"
 python manage.py createsuperuser
