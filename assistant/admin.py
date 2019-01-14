d3191e82afa5e22ad3fd34d2df8a712e9f51d94e
Julien LEICHER <jleicher@gmail.com>
2019-01-14 17:13:23 +0100

    Added settings

--- assistant/admin.py
+++ assistant/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Setting
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -10,9 +10,13 @@ class ProfileInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'profile'
 
+class SettingInline(admin.TabularInline):
+    model = Setting
+    extra = 1
+
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
-    inlines = (ProfileInline,)
+    inlines = (ProfileInline, SettingInline,)
 
 # Re-register UserAdmin
 admin.site.unregister(User)
--- assistant/models.py
+++ assistant/models.py
@@ -6,6 +6,11 @@ class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   language = models.CharField(max_length=10, default='fr-FR')
 
+class Setting(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=50)
+  value = models.TextField(default='')
+
 def create_user_profile(sender, instance, created, **kwargs):
   if created:
     Profile.objects.create(user=instance)
